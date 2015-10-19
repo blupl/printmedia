c763748aef1398a50718df0fad39a305335d1fe9
Ahsanul Kabir <ratulx00@gmail.com>
2015-10-19 20:36:12 +0600

    Unnecessary functions removed

--- src/Http/Controllers/ApprovalController.php
+++ src/Http/Controllers/ApprovalController.php
@@ -1,11 +1,7 @@
 <?php namespace Blupl\PrintMedia\Http\Controllers;
 
-use Blupl\PrintMedia\Http\Requests\Reporter;
-use Blupl\PrintMedia\Model\MediaOrganization;
 use Blupl\PrintMedia\Model\MediaReporter;
-use Blupl\PrintMedia\Model\Zone;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Input;
 use Blupl\PrintMedia\Processor\Approval as ApprovalProcessor;
 use Laracasts\Flash\Flash;
 use Orchestra\Foundation\Http\Controllers\AdminController;
@@ -39,10 +35,6 @@ class ApprovalController extends AdminController
 
     /**
      * Show a role.
-     *
-     * @param  int  $roles
-     *
-     * @return mixed
      */
     public function show($mediaCategory)
     {
@@ -68,44 +60,10 @@ class ApprovalController extends AdminController
         }
     }
 
-    /**
-     * Create a new role.
-     *
-     * @return mixed
-     */
-    public function create()
-    {
-        return $this->processor->create($this);
-    }
 
-    /**
-     * Edit the role.
-     *
-     * @param  int  $roles
-     *
-     * @return mixed
-     */
-     public function edit($medias)
-     {
-        return $this->processor->edit($this, $medias);
-     }
-
-    /**
-     * Create the role.
-     *
-     * @return mixed
-     */
-     public function store(Reporter $request)
-     {
-
-        return $this->processor->store($this, $request);
-     }
 
     /**
      * Update the role.
-     *
-     * @param  int  $roles
-     *
      * @return mixed
      */
     public function update($reporterId, Request $request)
@@ -137,179 +95,5 @@ class ApprovalController extends AdminController
 
     }
 
-    /**
-     * Request to delete a role.
-     *
-     * @param  int  $roles
-     *
-     * @return mixed
-     */
-    public function delete($medias)
-    {
-        return $this->destroy($medias);
-    }
-
-    /**
-     * Request to delete a role.
-     *
-     * @param  int  $roles
-     *
-     * @return mixed
-     */
-    public function destroy($medias)
-    {
-        return $this->processor->destroy($this, $medias);
-    }
-
-
-    /**
-     * Response when create role page succeed.
-     *
-     * @param  array  $data
-     *
-     * @return mixed
-     */
-    public function createSucceed()
-    {
-        set_meta('title', trans('blupl/printmedia::title.media.create'));
-        return view('blupl/printmedia::edit');
-    }
-
-    /**
-     * Response when edit role page succeed.
-     *
-     * @param  array  $data
-     *
-     * @return mixed
-     */
-    public function editSucceed(array $data)
-    {
-        set_meta('title', trans('blupl/printmedia::title.media.update'));
-
-        return view('blupl/printmedia::edit', $data);
-    }
-
-    /**
-     * Response when storing role failed on validation.
-     *
-     * @param  object  $validation
-     *
-     * @return mixed
-     */
-     public function storeValidationFailed($validation)
-     {
-         dd('Validation Error');
-//        return $this->redirectWithErrors(handles('orchestra::media/reporter'), $validation);
-     }
-
-    /**
-     * Response when storing role failed.
-     *
-     * @param  array  $error
-     *
-     * @return mixed
-     */
-     public function storeFailed(array $error)
-     {
-        $message = trans('orchestra/foundation::response.db-failed', $error);
-         dd('Validation Faild');
-
-//        return $this->redirectWithMessage(handles('orchestra::media/reporter'), $message);
-     }
-
-    /**
-     * Response when storing user succeed.
-     *
-     * @param  \Orchestra\Model\Role  $role
-     *
-     * @return mixed
-     */
-     public function storeSucceed(MediaOrganization $media)
-     {
-        $message = trans('blupl/printmedia::response.media.create', [
-//            'name' => $media->getAttribute('name')
-        ]);
-        dd('sss');
-//         return $this->redirectWithMessage(handles('orchestra::media/reporter'), $message);
-     }
-
-    /**
-     * Response when updating role failed on validation.
-     *
-     * @param  object  $validation
-     * @param  int     $id
-     *
-     * @return mixed
-     */
-     public function updateValidationFailed($validation, $id)
-     {
-        return $this->redirectWithErrors(handles("orchestra::media/reporter/{$id}/edit"), $validation);
-     }
-
-    /**
-     * Response when updating role failed.
-     *
-     * @param  array  $errors
-     *
-     * @return mixed
-     */
-     public function updateFailed(array $errors)
-     {
-        $message = trans('orchestra/foundation::response.db-failed', $errors);
-
-        return $this->redirectWithMessage(handles('orchestra::media/reporter'), $message);
-     }
-
-    /**
-     * Response when updating role succeed.
-     */
-    public function updateSucceed(media $media)
-    {
-        $message = trans('orchestra/control::response.roles.update', [
-            'name' => $media->getAttribute('name')
-        ]);
-
-        return $this->redirectWithMessage(handles('orchestra::media'), $message);
-    }
-
-    /**
-     * Response when deleting role failed.
-     *
-     * @param  array  $error
-     *
-     * @return mixed
-     */
-    public function destroyFailed(array $error)
-    {
-        $message = trans('orchestra/foundation::response.db-failed', $error);
-
-        return $this->redirectWithMessage(handles('orchestra::media'), $message);
-    }
-
-    /**
-     * Response when updating role succeed.
-     *
-     * @param  \Orchestra\Model\Role  $role
-     *
-     * @return mixed
-     */
-    public function destroySucceed(media $media)
-    {
-        $message = trans('orchestra/control::response.roles.delete', [
-            'name' => $media->getAttribute('name')
-        ]);
-
-        return $this->redirectWithMessage(handles('orchestra::media'), $message);
-    }
-
-    /**
-     * Response when user verification failed.
-     *
-     * @return mixed
-     */
-    public function userVerificationFailed()
-    {
-        return $this->suspend(500);
-    }
 
 }
